631ce874a080f24acb43ad23de446e5ec8dc3f80
zyzka <zuziaswis@gmail.com>
2025-03-18 18:19:45 +0100

    encoding password

--- src/main/java/com/example/projekt/service/UserService.java
+++ src/main/java/com/example/projekt/service/UserService.java
@@ -1,18 +1,29 @@
 package com.example.projekt.service;
 
 import com.example.projekt.model.RegularUser;
+import com.example.projekt.model.UserEntity;
 import com.example.projekt.repository.RegularUserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
     private final RegularUserRepository regularUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(RegularUserRepository regularUserRepository) {
+        this.regularUserRepository = regularUserRepository;
+        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    }
+
+    private void encodePassword(UserEntity user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    }
 
     public RegularUser createUser(RegularUser user) {
+        encodePassword(user);
         return regularUserRepository.save(user);
     }
 
