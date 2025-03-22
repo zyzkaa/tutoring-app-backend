0404f8d88629207847d8d4e55051781d04b98cd4
zyzka <zuziaswis@gmail.com>
2025-03-22 16:40:12 +0100

    change int to uuid

--- src/main/java/com/example/projekt/repository/TeacherRepository.java
+++ src/main/java/com/example/projekt/repository/TeacherRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
     Optional<Teacher> findByUsername(String username);
 }
--- src/main/java/com/example/projekt/repository/UserRepository.java
+++ src/main/java/com/example/projekt/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.projekt.repository;
 
 import com.example.projekt.model.User;
 import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 }
