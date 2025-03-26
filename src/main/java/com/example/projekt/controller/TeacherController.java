5118b86df19425ea84d822da4ea9f3c97f144a80
zyzka <zuziaswis@gmail.com>
2025-03-26 18:58:34 +0100

    can add description and subjects with prices to teachers

--- src/main/java/com/example/projekt/controller/TeacherController.java
+++ src/main/java/com/example/projekt/controller/TeacherController.java
@@ -1,8 +1,11 @@
 package com.example.projekt.controller;
 
-import com.example.projekt.dto.UserDto;
+import com.example.projekt.dto.TeacherDetailsDto;
+import com.example.projekt.dto.TeacherResponseDto;
+import com.example.projekt.dto.UserRegisterDto;
 import com.example.projekt.model.Teacher;
 import com.example.projekt.service.TeacherService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,9 +26,19 @@ public class TeacherController {
         return response;
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/get-all")
     public ResponseEntity<List<Teacher>> getAllTeacher() {
         return ResponseEntity.ok(teacherService.getAllTeachers());
     }
+
+    @PostMapping("/add-details")
+    public ResponseEntity<TeacherResponseDto> addTeacherDetails(@RequestBody TeacherDetailsDto teacherDetailsDto) {
+        return ResponseEntity.ok(teacherService.addDetails(teacherDetailsDto));
+    }
+
+//    @GetMapping("/get-with-data")
+//    public ResponseEntity<Teacher> getWithAllData() {
+//        return ResponseEntity.ok(teacherService.getWithAllData("asdasd"));
+//    }
 
 }
--- src/main/java/com/example/projekt/service/TeacherService.java
+++ src/main/java/com/example/projekt/service/TeacherService.java
@@ -1,23 +1,79 @@
 package com.example.projekt.service;
 
+import com.example.projekt.dto.TeacherDetailsDto;
+import com.example.projekt.dto.TeacherResponseDto;
+import com.example.projekt.exception.UsernameAlreadyExistsException;
+import com.example.projekt.model.SchoolPrice;
+import com.example.projekt.model.SubjectDetails;
 import com.example.projekt.model.Teacher;
-import com.example.projekt.repository.TeacherRepository;
+import com.example.projekt.repository.*;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TeacherService {
     private final TeacherRepository teacherRepository;
+    private final SubjectDetailsRepository subjectDetailsRepository;
+    private final SchoolPriceRepository schoolPriceRepository;
+    private final SchoolDictRepository schoolDictRepository;
+    private final SubjectDictRepository subjectDictRepository;
+    private final UserRepository userRepository;
 
-    public Teacher addTeacher(Teacher teacher) {
-        return teacherRepository.save(PasswordHelper.encodePassword(teacher));
+    public TeacherResponseDto addTeacher(Teacher teacherData) {
+        if(userRepository.existsByUsername(teacherData.getUsername())) {
+            throw new UsernameAlreadyExistsException(teacherData.getUsername());
+        }
+
+        Teacher teacher =  teacherRepository.save(PasswordHelper.encodePassword(teacherData));
+        return new TeacherResponseDto(teacher);
     }
 
     public List<Teacher> getAllTeachers() {
         return teacherRepository.findAll();
     }
 
+
+    @Transactional
+    public TeacherResponseDto addDetails(TeacherDetailsDto teacherDetailsDto) {
+        Teacher teacher = teacherRepository.findByUsername("asdasd")
+                .orElseThrow(() -> new EntityNotFoundException("Teacher not found"));
+
+        teacher.setDescription(teacherDetailsDto.getDescription());
+
+        var subjectList = new ArrayList<SubjectDetails>();
+        teacherDetailsDto.getSubjects().forEach(subject -> {
+            var schoolPriceList = new ArrayList<SchoolPrice>();
+
+            subject.getSchoolPrices().forEach(schoolPrice -> {
+                var newSchoolPrice = new SchoolPrice(
+                        schoolDictRepository.findById(schoolPrice.getSchoolId())
+                                .orElseThrow(() -> new EntityNotFoundException("School id not found")),
+                        schoolPrice.getPrice()
+                );
+
+                schoolPriceRepository.save(newSchoolPrice);
+                schoolPriceList.add(newSchoolPrice);
+            });
+
+            var newSubject = new SubjectDetails(
+                    subjectDictRepository.findById(subject.getSubjectId())
+                            .orElseThrow(() -> new EntityNotFoundException("Subject id not found")),
+                    schoolPriceList,
+                    teacher
+            );
+
+            subjectDetailsRepository.save(newSubject);
+            subjectList.add(newSubject);
+        });
+        teacher.setSubjectDetails(subjectList);
+
+        teacherRepository.save(teacher);
+        return new TeacherResponseDto(teacher);
+    }
 }
