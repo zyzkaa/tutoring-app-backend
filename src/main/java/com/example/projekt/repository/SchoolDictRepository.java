28616c215bcfeead33ffea7b3123146930c25390
zyzka <zuziaswis@gmail.com>
2025-03-26 18:52:30 +0100

    added subjects and prices repositories

---
+++ src/main/java/com/example/projekt/repository/SchoolDictRepository.java
@@ -0,0 +1,7 @@
+package com.example.projekt.repository;
+
+import com.example.projekt.model.SchoolDict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolDictRepository extends JpaRepository<SchoolDict, Integer> {
+}
